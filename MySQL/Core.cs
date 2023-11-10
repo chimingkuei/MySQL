a2475dd3fe5b2098c8287cd11392611a054227b3
chimingkuei <chimingkuei@gmail.com>
2023-11-10 23:15:09 +0800

    MySQL

---
+++ MySQL/Core.cs
@@ -0,0 +1,126 @@
+﻿using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Relational;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MySQL
+{
+    class Core
+    {
+        public string port { get; set; }
+        public string user { get; set; }
+        public string password { get; set; }
+
+        public Core(string _port, string _user, string _password)
+        {
+            port = _port;
+            user = _user;
+            password = _password;
+        }
+
+        private string StrPack()
+        {
+            return "server=127.0.0.1;" + nameof(port) + "=" + port + ";" + nameof(user) + "=" + user + ";" + nameof(password) + "=" + password + ";";
+        }
+
+        #region Database Operation
+        public string CreateDatabase(string database_name)
+        {
+            return "create database " + database_name + ";";
+        }
+
+        public string DropDatabase(string database_name)
+        {
+            return "drop database " + database_name + ";";
+        }
+
+        public void MySQLDatabase(string database_name, Func<string, string> fun)
+        {
+            string connetStr = StrPack();
+            MySqlConnection conn = new MySqlConnection(connetStr);
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(fun(database_name), conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+        #endregion
+
+        #region Table Operation
+        public string CreateTable(string database_name, string table_name, string content)
+        {
+            return "use " + database_name + ";" +
+                   "create table " + table_name + "(" + content + ");";
+        }
+
+        public string AddTableColumn(string database_name, string table_name, string content)
+        {
+            return "use " + database_name + ";" +
+                   "alter table " + table_name + "add" + "(" + content + ");";
+        }
+
+        public string DeleteTableColumn(string database_name, string table_name, string column)
+        {
+            return "use " + database_name + ";" +
+                   "alter table " + table_name + "drop" + column + ";";
+        }
+
+        public string AddTableData(string database_name, string table_name, string data)
+        {
+            return "use " + database_name + ";" +
+                   "insert into " + table_name + "values" + "(" + data + ");";   
+        }
+
+        public string DeleteTableData(string database_name, string table_name, string condition)
+        {
+            return "use " + database_name + ";" +
+                   "delete from " + table_name + "where" + condition + ";";
+        }
+
+        public string DropTable(string database_name, string table_name, string content)
+        {
+            return "use " + database_name + ";" +
+                   "drop table " + table_name + ";";
+        }
+
+        public void MySQLTable(string database_name, string table_name, string content, Func<string, string, string, string> fun)
+        {
+            string connetStr = StrPack();
+            MySqlConnection conn = new MySqlConnection(connetStr);
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(fun(database_name, table_name, content), conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+        #endregion
+
+        
+
+
+    }
+}
---
+++ MySQL/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using MySql.Data.MySqlClient;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MySQL
+{
+    public class Parameter
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+    }
+    
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Function
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("請問是否要關閉？", "確認", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+        #endregion
+
+        #region Parameter and Init
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #region Log
+        BaseLogRecord Logger = new BaseLogRecord();
+        //Logger.WriteLog("儲存參數!", 1, richTextBoxGeneral);
+        #endregion
+        #region Config
+        BaseConfig<Parameter> Config = new BaseConfig<Parameter>();
+        //Load Config
+        //List<Parameter> Parameter_info = Config.Load();
+        //Console.WriteLine(Parameter_info[0].ID);
+        //Save Config
+        //List<Parameter> Parameter_config = new List<Parameter>()
+        //{
+        //    new Parameter() { ID = 1, Name = "張飛"}
+        //};
+        //Config.Save(Parameter_config);
+        #endregion
+        #endregion
+
+        #region Main Screen
+        private void Main_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as Button).Name)
+            {
+                case nameof(Demo):
+                    {
+                        Core Do = new Core("3306", "root", "Asher19910930");
+                        //Do.MySQLDatabase("school2", Do.CreateDatabase);
+                        //Do.MySQLTable("school", "classc", "", Do.DropTable);
+                        break;
+                    }
+               
+            }
+        }
+        #endregion
+
+
+
+
+
+    }
+}
